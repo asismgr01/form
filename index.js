da79c2a31fe30d808b8fbbeeb8a94f6d161cd1d6
Ashish Thapa Magar <ashishthapamagar987@gmail.com>
2021-04-11 13:44:49 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let tr = doucment.createElement("tr");
+let students = [
+  {
+    name: "Ashish",
+    roll_no: 6,
+    age: 24,
+    address: "Pokhara,Srijana Chowk",
+  },
+  {
+    name: "Ashim",
+    roll_no: 4,
+    age: 24,
+    address: "Pokhara,Lamachour",
+  },
+  {
+    name: "Resham",
+    roll_no: 12,
+    age: 25,
+    address: "Pokhara,Chorepatan",
+  },
+  {
+    name: "Sankhar",
+    roll_no: 23,
+    age: 24,
+    address: "Pokhara,Batulechour",
+  },
+  {
+    name: "Sushan",
+    roll_no: 34,
+    age: 32,
+    address: "Pokhara,Gairapatan",
+  },
+];
+/*
+students.forEach((data) => {
+  let td = createElement("td");
+  td.innerHtml = data.name;
+  tr.append
+});
+*/
